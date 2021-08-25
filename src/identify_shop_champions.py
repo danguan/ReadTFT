468c012997655dfe6569f4a9e0cd694c1bd9c2d9
Daniel Guan <daniel.guan.bca@gmail.com>
2021-08-25 00:12:46 -0400

    Add helpers for identifying ROIs
    - TODO: Fill out rest of resolutions in mapping
    - TODO: Do best-estimate guess if resolution is not found

--- src/identify_shop_champions.py
+++ src/identify_shop_champions.py
@@ -8,12 +8,70 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+from typing import Tuple, TypedDict
+
 try:
     from PIL import Image
 except ImportError:
     import Image
 
 import pytesseract
+
+
+class Resolution(TypedDict):
+    width: int
+    height: int
+
+
+def _get_shop_champs_roi_height(resolution: Resolution) -> Tuple[int, int]:
+    """Returns shop's champion names ROI height based on resolution.
+
+    Height is assumed to be uniform, so output tuple contains the lower/upper
+    bounds of all champions' name heights.
+
+    Args:
+        resolution: Resolution object containing source width/height of image.
+
+    Returns:
+        Tuple containing the lower, then upper boundaries of the image's
+        region of interest, i.e. champions' names only, excluding gold value.
+    """
+    resolution_height_roi_mapping = {
+        (1680, 1001): (965, 990),
+    }
+    resolution_wh = (resolution["width"], resolution["height"])
+
+    if resolution_wh in resolution_height_roi_mapping:
+        return resolution_height_roi_mapping[resolution_wh]
+    return (0, 0)  # TODO(Dan): Return best estimate if resolution isn't mapped
+
+
+def _get_shop_champs_roi_width_interval(
+    resolution: Resolution,
+) -> Tuple[int, int, int]:
+    """Returns shop's champion names ROI width/interval based on resolution.
+
+    Function will specifically return the left/right bounds of the first
+    champion name in the shop, with the interval being the distance between
+    the left bound of the first champion name, and the left bound of the second
+    champion name.
+
+    Args:
+        resolution: Resolution object containing source width/height of image.
+
+    Returns:
+        Tuple containing the left, then right bounds of the first champion name
+        in the shop, followed by the distance between the start of the first
+        champion name and the second champion name.
+    """
+    resolution_width_roi_mapping = {
+        (1680, 1001): (398, 535, 187),
+    }
+    resolution_wh = (resolution["width"], resolution["height"])
+
+    if resolution_wh in resolution_width_roi_mapping:
+        return resolution_width_roi_mapping[resolution_wh]
+    return (0, 0)  # TODO(Dan): Return best estimate if resolution isn't mapped
 
 
 def main():
